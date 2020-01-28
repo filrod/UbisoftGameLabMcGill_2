20592abb00598b817f81c9dad728bf91db212e28
Robin Leman <robin.leman@mail.mcgill.ca>
2020-01-28 17:01:42 -0500

    Player movement fixed. First initialization of Player Collision.
    - First version still with errors

---
+++ DuKoup/Assets/Scripts/Collision.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * UBISOFT GAMES LAB - McGill Team #2
+ * -----------------------------------
+ * 
+ * @author Robin Leman
+ * @Date 2020/01/28
+ * 
+ * This class controls players collision and plane changes
+*/
+public class Collision : MonoBehaviour
+{
+    // Fields
+
+    // Two players, instancePlayer has the script attached
+    [SerializeField] private Rigidbody instancePlayer;
+    [SerializeField] private Rigidbody otherPlayer;
+
+    // Separate the level into 2 plains, with a distance diffPlane between the two planes. The initial plane should be the main one, alpha.
+    [SerializeField] private float diffPlane;
+
+    // Collision area attribute
+    [SerializeField] private float collisionRadius;    // Should be the width of the player
+    private float areaPositionMax;
+    private float areaPositionMin;
+    private float playerPos;
+
+    void Update()
+    {
+
+        // Create a collision area for otherPlayer
+        // 2D area, with the radius of the size of the players, centered on otherPlayer.
+            // Be sure that every players are in Alpha plane
+        // If instancePlayer collides with collision area of otherPlayer, while he is in this area he moves to Beta plane
+        // When instancePlayer goes out of the collision area, he moves back to alpha plane
+
+        areaPositionMax = otherPlayer.transform.position.x + collisionRadius;
+        areaPositionMin = otherPlayer.transform.position.x - collisionRadius;
+        playerPos = instancePlayer.transform.position.x;
+        currentPlane = instancePlayer.transform.position.z;
+
+        if ( currentPlane == alphaPlane && (playerPos >= areaPositionMin && playerPos <= areaPositionMax))
+        {
+
+            instancePlayer.transform.position.z += new Vector3(0, 0, diffPlane); // Move player into beta Plane to avoid collision
+        }
+
+        if ( currentPlane == betaPlane && (playerPos <= areaPositionMin || playerPos >= areaPositionMax))
+        {
+            instancePlayer.transform.position.z += new Vector3(0, 0, -diffPlane); // Move player back into the main plane
+        }
+    }
+}
--- DuKoup/Assets/Scripts/Movement.cs
+++ DuKoup/Assets/Scripts/Movement.cs
@@ -11,28 +11,47 @@ using UnityEngine;
  * 
  * This class controls player movement
 */
+
 public class Movement : MonoBehaviour
 {
-    public Rigidbody player;
-    public float speed = 0.2f;
+    // Fields 
 
-    float horizontalMovement;
-    Vector3 movement;
+    [SerializeField] private int playerId;
+    [SerializeField] private Rigidbody player;
+    [SerializeField] private float speed = 10f;
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
+
+    private float horizontalMovement;
+    private Vector3 movement;
+    private string axis;
+    private Vector3 m_Velocity = Vector3.zero;
 
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         horizontalMovement = 0;
         movement = new Vector3(horizontalMovement, 0f, 0f);
 
+        if (playerId == 1)
+            axis = "Horizontal1";
+        else if (playerId == 2)
+            axis = "Horizontal2";
     }
 
     // Update is called once per frame
-    void Update()
+    public void FixedUpdate()
     {
-        horizontalMovement += Input.GetAxis("Horizontal");
-        movement = new Vector3(horizontalMovement * speed, 0f, 0f);
-        player.transform.position += movement;
+        horizontalMovement = Input.GetAxis(axis) * speed;
+
+        // Move the character by finding the target velocity
+        Vector3 targetVelocity = new Vector2(horizontalMovement, player.velocity.y);
+        // And then smoothing it out and applying it to the character
+        player.velocity = Vector3.SmoothDamp(player.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+    }
+
+    public int getPlayerId()
+    {
+        return this.playerId;
     }
 }
