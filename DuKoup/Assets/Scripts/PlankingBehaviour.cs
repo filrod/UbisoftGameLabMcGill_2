cdba28148c5c93a27d901fda4066f9618ed5e571
rikkeaas <rikrikaas@gmail.com>
2020-01-30 13:43:43 -0500

    Added some logic

--- DuKoup/Assets/Scripts/PlankingBehaviour.cs
+++ DuKoup/Assets/Scripts/PlankingBehaviour.cs
@@ -4,5 +4,43 @@ using UnityEngine;
 
 public class PlankingBehaviour : MonoBehaviour
 {
+
+    private bool inPlankinggSpace = false;
+    private GameObject plankingSpace;
+
+
+    void FixedUpdate()
+    {
+        if (inPlankinggSpace && Input.GetKeyDown(KeyCode.P))
+        {
+            Plank();
+        }
+    }
+
+    private void Plank()
+    {
+        transform.position = plankingSpace.transform.position;
+        transform.eulerAngles = new Vector3(90, 0, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collided");
+        if (other.CompareTag("PlankingSpace"))
+        {
+            Debug.Log("Entered planking space");
+            inPlankinggSpace = true;
+            plankingSpace = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("PlankingSpace"))
+        {
+            inPlankinggSpace = false;
+            plankingSpace = null;
+        }
+    }
     
 }
