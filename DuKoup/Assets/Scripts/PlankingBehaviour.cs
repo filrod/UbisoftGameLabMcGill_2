525761583d950c0ac6eafcb1e504d691a7c3031e
rikkeaas <rikrikaas@gmail.com>
2020-01-31 14:15:59 -0500

    Fixed the planking space script, much simpler now

--- DuKoup/Assets/Scripts/PlankingBehaviour.cs
+++ DuKoup/Assets/Scripts/PlankingBehaviour.cs
@@ -7,9 +7,9 @@ using UnityEngine;
  * -----------------------------------
  * 
  * @author Rikke Aas
- * @Date 2020/01/30
+ * @Date 2020/01/31
  * 
- * This class controls the planking behaviour
+ * This class controls the planking behaviour for the player with plaking ability
 */
 
 public class PlankingBehaviour : MonoBehaviour
@@ -33,41 +33,46 @@ public class PlankingBehaviour : MonoBehaviour
         }
     }
 
-    private void UnPlank()
-    {
-        isPlanking = false;
-        Debug.Log(positionBeforePlank);
-        transform.position = positionBeforePlank;
-        positionBeforePlank = Vector3.zero;
-        transform.eulerAngles = new Vector3(0, 0, 0);
-        transform.localScale = new Vector3(1, 1, 1);
-    }
-
     private void Plank()
     {
         isPlanking = true;
-        Debug.Log(transform.position);
-        positionBeforePlank = transform.position;
-        transform.position = new Vector3(plankingSpace.transform.position.x, 0, 0);
+        plankingSpace.GetComponent<PlankingSpace>().TurnOnBridge(); // Turns on bridge collider of planking space
+        
+        positionBeforePlank = transform.position; // Storing the position of the player to be able to move it back after planking is done
 
+        // The following three lines probably won't stay like this when we get animations.
+        // Mostly just for visuals, the other player doesn't actually use this player as a bridge (it is a sepparate collider)
+        transform.position = new Vector3(plankingSpace.transform.position.x, 0, 0);
         transform.eulerAngles = new Vector3(0, 0, 90);
         transform.localScale = new Vector3(0.1f, 4f, 1f);
     }
 
+    private void UnPlank()
+    {
+        isPlanking = false;
+        plankingSpace.GetComponent<PlankingSpace>().TurnOffBridge(); // Turning of bridge collider of the planking space
+        
+        // For the following three lines: see comments for similar lines in Plank()
+        transform.position = positionBeforePlank;
+        transform.eulerAngles = new Vector3(0, 0, 0);
+        transform.localScale = new Vector3(1, 1, 1);
+        
+        positionBeforePlank = Vector3.zero; // Resetting the postion (shouldn't be necessaary, but just in case)
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Collided");
-        if (other.CompareTag("PlankingSpace"))
+        if (other.CompareTag("PlankingSpace")) // Detecting when player enters the planking space
         {
             Debug.Log("Entered planking space");
             inPlankinggSpace = true;
-            plankingSpace = other.gameObject;
+            plankingSpace = other.gameObject; // Storing reference to the planking space, this lets player turn on and off bridge when planking
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("PlankingSpace"))
+        if (other.CompareTag("PlankingSpace")) // Detecting when player leaves planking space
         {
             Debug.Log("Leaving planking space");
             inPlankinggSpace = false;
@@ -77,9 +82,9 @@ public class PlankingBehaviour : MonoBehaviour
 
     void OnCollisionEnter(Collision collisionInfo)
     {
-        if (!inPlankinggSpace) return;
+        if (!isPlanking) return; // Only want to ignore collision when player is planking (can only happen in planking space)
 
-        if (collisionInfo.gameObject.CompareTag("Player"))
+        if (collisionInfo.gameObject.CompareTag("Player")) // Only ignoring collisions between players
         {
             Physics.IgnoreCollision(collisionInfo.gameObject.GetComponent<Collider>(), GetComponent<Collider>());
         }
