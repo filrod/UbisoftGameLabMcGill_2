6c9373d6010027e2506b24487f29d56fb70c6d98
Chicheng <zy22092@nottingham.edu.cn>
2020-01-31 12:45:24 -0500

    Add Photon package and test connect

---
+++ DuKoup/Assets/TestConnect.cs
@@ -0,0 +1,30 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestConnect : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Connecting to server");
+        PhotonNetwork.GameVersion = "0.0.1";
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        print("Connecting to master");
+        
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        base.OnDisconnected(cause);
+        print("Connecting to master");
+    }
+
+}
