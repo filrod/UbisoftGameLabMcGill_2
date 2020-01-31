9017777cfc6ddd43cc033eed85b30d7d80e4691a
filrod <39252535+filrod@users.noreply.github.com>
2020-01-31 16:42:02 -0500

    Code for the jumping mechanic but no staking. Also has some starter enemy code

--- DuKoup/Assets/Scripts/PlayerMovement.cs
+++ DuKoup/Assets/Scripts/PlayerMovement.cs
@@ -19,33 +19,83 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private int playerId;
     [SerializeField] private Rigidbody player;
     [SerializeField] private float speed = 10f;
-    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
 
-    private float horizontalMovement;
-    private Vector3 movement;
-    private string axis;
+    [Range(0.0f, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
+    [Range(5.0f, 15.0f)] [SerializeField] private float jumpVelocity = 7.0f;
+    [Range(0.0f, 6.0f)] [SerializeField] private float gravityMultiplier = 4.5f;
+    [Range(0.0f, 6.0f)] [SerializeField] private float lowJumpGravityMultiplier = 2f;
+
+
+    private Vector2 movementXY;
+    private string horizontalAxis;
+    private string jumpButton;
     private Vector3 m_Velocity = Vector3.zero;
 
+    private const float g = 9.81f;
+    private const float averageHumanJump = 2.5f; // Times your mass on earth
 
-    // Start is called before the first frame update
+
+    /**
+     * 
+     * This method distinguishes which objet is using this 
+     * class and assigns an axis configuration based on the
+     * playerId
+     * 
+     * Initiates horizontalMovement to be zero for use in 
+     * FixedUpdate()
+     */
     public void Start()
     {
-        horizontalMovement = 0;
-        movement = new Vector3(horizontalMovement, 0f, 0f);
+        movementXY = new Vector2(0, 0);
 
         if (playerId == 1)
-            axis = "Horizontal1";
+        {
+            horizontalAxis = "Horizontal1";
+            jumpButton = "Vertical1";
+        }
         else if (playerId == 2)
-            axis = "Horizontal2";
+        {
+            horizontalAxis = "Horizontal2";
+            jumpButton = "Vertical2";
+        }
     }
 
-    // Update is called once per frame
+    /**
+     * FixedUpdate() gets called twice per frame (around 50 
+     * times per second) and is best used to compute values 
+     * for physics.
+     * 
+     * 
+     */
     public void FixedUpdate()
     {
-        horizontalMovement = Input.GetAxis(axis) * speed;
+        movementXY.x = Input.GetAxis(horizontalAxis) * speed;
+        movementXY.y = 0;
+        if (Input.GetButtonDown(jumpButton))
+        {
+            
+            //player.AddForce(transform.up * player.mass * g * averageHumanJump*15f*Time.deltaTime, ForceMode.Impulse);
+            player.velocity += Vector3.up * jumpVelocity;
+
+            // This next part makes jumps more videogame-like
+
+            // If player is falling back down
+            if (player.velocity.y < 0)
+            {
+                player.velocity += Vector3.up * Physics.gravity.y * (gravityMultiplier - 1) * Time.deltaTime;
+                //Debug.Log("Falling");
+            }
+            // If player is going up in the jump and not still holding jump button down
+            else if (player.velocity.y > 0 && !Input.GetButton(jumpButton))
+            {
+                player.velocity += Vector3.up * Physics.gravity.y * (lowJumpGravityMultiplier - 1) * Time.deltaTime;
+                //Debug.Log("Low jump");
+            }
+        }
+
 
         // Move the character by finding the target velocity
-        Vector3 targetVelocity = new Vector2(horizontalMovement, player.velocity.y);
+        Vector3 targetVelocity = new Vector2(movementXY.x, player.velocity.y);
         // And then smoothing it out and applying it to the character
         player.velocity = Vector3.SmoothDamp(player.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
     }
@@ -54,4 +104,5 @@ public class PlayerMovement : MonoBehaviour
     {
         return this.playerId;
     }
+
 } 
---
+++ DuKoup/Assets/Scripts/enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    bool attack()
+    {
+        bool hit = false;
+
+        // Attack
+
+        // Check if hit
+
+        return hit;
+    }
+}
