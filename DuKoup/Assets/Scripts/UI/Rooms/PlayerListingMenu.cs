22f7565749c05efe0fc9692a2173f6b050b24c64
Chicheng <zy22092@nottingham.edu.cn>
2020-02-01 14:28:08 -0500

    Fix bug incorrect player on new room

--- DuKoup/Assets/Scripts/UI/Rooms/PlayerListingMenu.cs
+++ DuKoup/Assets/Scripts/UI/Rooms/PlayerListingMenu.cs
@@ -3,6 +3,7 @@ using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerListingMenu : MonoBehaviourPunCallbacks
 {
@@ -10,13 +11,42 @@ public class PlayerListingMenu : MonoBehaviourPunCallbacks
     private Transform _content;
     [SerializeField]
     private PlayerListing _playerListing;
+    [SerializeField]
+    private Text _readyUpText;
 
     private List<PlayerListing> _listings = new List<PlayerListing>();
     private RoomsCanvases _roomsCanvases;
+    private bool _ready = false;
 
-    private void Awake()
+    public override void OnEnable()
     {
+        base.OnEnable();
+        SetReadyUp(false);
         GetCurrentRoomPlayers();
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        for (int i = 0; i < _listings.Count; i++)
+        {
+            Destroy(_listings[i].gameObject);
+        }
+
+        _listings.Clear();
+    }
+
+    public void SetReadyUp(bool state)
+    {
+        _ready = state;
+        if (_ready)
+        {
+            _readyUpText.text = "R";
+        }
+        else
+        {
+            _readyUpText.text = "T";
+        }
     }
 
     public void FirstInitialize(RoomsCanvases canvases)
@@ -24,14 +54,23 @@ public class PlayerListingMenu : MonoBehaviourPunCallbacks
         _roomsCanvases = canvases;
     }
 
-    public override void OnLeftRoom()
-    {
-        base.OnLeftRoom();
-        _content.DestroyChildren();
-    }
+    //public override void OnLeftRoom()
+    //{
+    //    base.OnLeftRoom();
+    //    _content.DestroyChildren();
+    //}
 
     private void GetCurrentRoomPlayers()
     {
+        if (!PhotonNetwork.IsConnected)
+        {
+            return;
+        }
+        if (PhotonNetwork.CurrentRoom == null || PhotonNetwork.CurrentRoom.Players == null)
+        {
+            return;
+        }
+
         foreach (KeyValuePair<int, Player> playerInfo in PhotonNetwork.CurrentRoom.Players)
         {
             AddPlayerListing(playerInfo.Value);
@@ -40,11 +79,19 @@ public class PlayerListingMenu : MonoBehaviourPunCallbacks
 
     private void AddPlayerListing(Player player)
     {
-        PlayerListing listing = Instantiate(_playerListing, _content);
-        if (listing != null)
+        int index = _listings.FindIndex(x => x.Player == player);
+        if (index != -1)
         {
-            listing.SetPlayerInfo(player);
-            _listings.Add(listing);
+            _listings[index].SetPlayerInfo(player);
+        }
+        else
+        {
+            PlayerListing listing = Instantiate(_playerListing, _content);
+            if (listing != null)
+            {
+                listing.SetPlayerInfo(player);
+                _listings.Add(listing);
+            }
         }
     }
 
