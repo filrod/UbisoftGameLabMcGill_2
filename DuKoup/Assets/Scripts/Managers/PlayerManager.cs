175c1fd1c487e57e4ae907084ab3440cb6de31ed
Robin Leman <robin.leman@mail.mcgill.ca>
2020-02-10 18:20:32 -0500

    Grabbable dragging disabled temporarly

---
+++ DuKoup/Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    // Not a singleton : two players
+
+    // Fields
+    [SerializeField] private int strength;
+
+    public int getStrength(){
+        return this.strength;
+    }
+
+    public void addStrength(int toAdd){
+        this.strength += toAdd;
+    }
+
+    public void decreaseStrength(int toDecre){
+        this.strength -= toDecre;
+    }
+
+    public void changeStrength(int new_strength){
+        this.strength = new_strength;
+    }
+}
