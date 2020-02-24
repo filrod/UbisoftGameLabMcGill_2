965f5d806454b2679f217fb340105e7485c5990e
Robin Leman <robin.leman@mail.mcgill.ca>
2020-02-23 19:04:16 -0500

    Revert "added quit option"
    This reverts commit fb38d74450b8bcba4de93f7acefaaa45477d67ee.

--- DuKoup/Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ DuKoup/Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -195,9 +195,6 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F4)){
             Application.LoadLevel(0);
         }
-        if (Input.GetKeyDown(KeyCode.F5)){
-            Application.Quit();
-        }
     }
 
     public bool CheckIfGrounded()
