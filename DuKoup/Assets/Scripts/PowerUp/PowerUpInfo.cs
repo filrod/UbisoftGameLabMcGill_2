95aac5eb4b0ee6304045717319f597daa10270f9
Chicheng <zy22092@nottingham.edu.cn>
2020-03-04 16:08:07 -0500

    Auto stash before merge of "Chicheng" and "origin/filrod_vs2"
    power up spot manager

---
+++ DuKoup/Assets/Scripts/PowerUp/PowerUpInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PowerUpInfo
+{
+
+    [SerializeField]
+    public Vector3 position = new Vector3(0, 0, 0);
+
+    [SerializeField]
+    public Quaternion rotation = Quaternion.identity;
+
+    public enum POWER_TYPE
+    {
+        Low_Gravity,
+        Fast_Speed,
+        Grabable,
+        High_Jump,
+        Double_Jump,
+        Default
+    }
+
+    [SerializeField]
+    private POWER_TYPE currentPowerType = POWER_TYPE.Default;
+
+    public POWER_TYPE CURRENT_TYPE
+    {
+        get
+        {
+            return currentPowerType;
+        }
+        set
+        {
+            currentPowerType = value;
+        }
+    }
+}
