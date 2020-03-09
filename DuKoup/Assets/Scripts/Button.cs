1e24966241422d7e6a55c53256060983c8dc1bc5
rikkeaas <rikrikaas@gmail.com>
2020-03-08 22:28:53 -0400

    Made button with animation when player jumps on it

---
+++ DuKoup/Assets/Scripts/Button.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+
+    Animator animator => GetComponent<Animator>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("PushingDown", true);
+            ButtonAction();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("PushingDown", false);
+            ButtonUnAction();
+        }
+    }
+
+    private void ButtonAction()
+    {
+        Debug.Log("Pressed button");
+    }
+
+    private void ButtonUnAction()
+    {
+        Debug.Log("Unpressed button");
+    }
+}
