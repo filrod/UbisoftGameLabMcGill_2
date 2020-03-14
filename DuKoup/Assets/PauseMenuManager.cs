80133ca764d547750f50ebb36181e3fb3bfa49f5
Robin Leman <robin.leman@mail.mcgill.ca>
2020-03-13 23:09:42 -0400

    Pause Menu WIP

---
+++ DuKoup/Assets/PauseMenuManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuCanvas;
+    [SerializeField] private string currScene;
+
+    private bool isPause = false;
+
+    public void Continue()
+    {
+        Time.timeScale = 1f;
+        pauseMenuCanvas.SetActive(false);
+        isPause = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(currScene);
+    }
+
+    public void Settings()
+    {
+        Debug.Log("Entered settings");
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    void Update()
+    {
+        if (!isPause && Input.GetButtonDown("Cancel"))
+        {
+            pauseMenuCanvas.SetActive(true);
+            Time.timeScale = 0f;
+            isPause = true;
+        }
+
+        if (isPause && (Input.GetButtonDown("Cancel") || Input.GetButtonDown("Back")) )
+        {
+            Continue();
+        }  
+    }
+}
