b553094f67a81c998455cfa036949fc551a32e5a
Filipe Rodrigues <filipe.rodrigues@mail.mcgill.ca>
2020-03-14 20:11:48 -0400

    New method: Scientist now clibs up and down stairs to investigate
    A climb state has to be made.

--- DuKoup/Assets/Scripts/AI/Scientist.cs
+++ DuKoup/Assets/Scripts/AI/Scientist.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Scientist : MonoBehaviour
 {
@@ -16,6 +17,27 @@ public class Scientist : MonoBehaviour
     public StateMachine stateMachine => GetComponent<StateMachine>();
     private Vector3? targetPosition = null; // ? indicates a nullable type. If it has a value of null that means the ai has no target and should be wandering
     private Vector3? nextTargetPosition;
+
+    // For climbing stairs -----------
+    private int xForSinWaveIncrementable = 0;
+
+    [SerializeField]
+    [Tooltip("How fast the scientist bobs up and down in rotations per sec")]
+    [Range(0f, 2.2f)]
+    private float occilationFreq = 0.8f;
+
+    [SerializeField]
+    [Tooltip("How fast the scientist climbs up the stairs. 0.8 is slow and suspensful 1.016 is quick like he's annoyed and investigating.")]
+    [Range(0f, 0.5f)]
+    private float stepHeight = 0.09f;
+
+    [SerializeField]
+    [Tooltip("Percentage of distance to increment lerp by when climbing stairs")]
+    [Range(0f, 0.1f)]
+    private float percentLerpDelta = 0.1f;
+
+    // ------------
+
     /// <summary>
     /// Main Camera used to set scientist movement bounds 
     /// </summary>
@@ -40,6 +62,36 @@ public class Scientist : MonoBehaviour
     public Vector2 GetZMovementBounds()
     {
         return new Vector2(scientistZAtTable, navmeshBackZ);
+    }
+
+    /// <summary>
+    /// Moving up and down stairs 
+    /// Don't forget to disable and enable the nav mesh if ascending or descending respectively
+    /// call: this.GetComponent<NavMeshAgent>().enabled = false; before entering the UsingStairesState    
+    /// </summary>
+    /// <param name="from"></param>
+    /// <param name="to"></param>
+    /// <param name="stepHeight"></param>
+    public void MoveOnStairs(Vector3 from, Vector3 to, float stepHeight)
+    {
+        /// Don't forget to disable and enable the nav mesh if ascending or descending respectively
+        /// call: this.GetComponent<NavMeshAgent>().enabled = false; before entering the UsingStairesState
+        /// 
+        stepHeight = Mathf.Abs(stepHeight);
+        if (Mathf.Abs(from.y - to.y) < stepHeight) return;
+
+        // If moving down
+        if (from.y > to.y) stepHeight *= -1f;
+
+        float delta = percentLerpDelta;// / (Mathf.Abs((to - from).magnitude));
+        float fullCircle = 2f * Mathf.PI / 50;
+
+        Vector3 lerpedVec3 = Vector3.Lerp(from, to, delta); // Comment out if lerp unwanted
+        lerpedVec3 = new Vector3(lerpedVec3.x, this.transform.position.y, lerpedVec3.z);  // Comment out if lerp unwanted
+
+        Vector3 sinMovement = Vector3.up * stepHeight * Mathf.Abs(Mathf.Sin(xForSinWaveIncrementable * fullCircle * occilationFreq));
+        this.transform.position = lerpedVec3;  // Comment out if lerp unwanted
+        this.transform.position += sinMovement;
     }
 
     /// <summary>
@@ -70,6 +122,20 @@ public class Scientist : MonoBehaviour
     private void Update()
     {
         scientistRenderer.material.color = stateMachine.stateColor;
+    }
+
+    /// <summary>
+    /// Only for scientist vertical movement when climbing stairs
+    /// Increment xForSinWaveIncrementable for the climbing stairs
+    /// </summary>
+    private void FixedUpdate()
+    {
+        // Important for sin function
+        xForSinWaveIncrementable++;
+
+        /// Disable nav mesh and uncomment this for a demo
+        /// MoveOnStairs(this.transform.position, new Vector3(28.4f, 29.3f, 21.7f), this.stepHeight);  // erase this
+
     }
 
     // Returns null if the scientist is not triggered
